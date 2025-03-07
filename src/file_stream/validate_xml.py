f06353aa4bb96c7f51ac4814d45bdb72cd9f1beb
beclarkkv <beclark@kinetic-vision.com>
2025-03-07 14:45:11 -0500

    laid out framework to validate the versions element

---
+++ src/file_stream/validate_xml.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
+
+def validate_versions(root: Element) -> bool:
+    pass
+
+def validate_transferProtocol(transferProtocol: Element) -> bool:
+    pass
+
+def validate_application(application: Element) -> bool:
+    pass
+
+def verify_attrib(element: Element, allowed_attrib: dict) -> bool:
+    pass
